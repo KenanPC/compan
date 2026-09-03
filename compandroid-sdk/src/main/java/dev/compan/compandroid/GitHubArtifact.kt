9623116eddc64f499e271bf57a883d28ce5c4e82
Kenan Courtney <kenan.courtney@gmail.com>
2026-09-03 11:31:40 -0400

    Document artifact identity fields

--- compandroid-sdk/src/main/java/dev/compan/compandroid/GitHubArtifact.kt
+++ compandroid-sdk/src/main/java/dev/compan/compandroid/GitHubArtifact.kt
@@ -1,9 +1,9 @@
 package dev.compan.compandroid
 
+// Preserve workflow run identity so repeated "not newer" checks can be skipped.
 internal data class GitHubArtifact(
     val name: String,
     val downloadUrl: String,
     val workflowRunId: Long,
     val headSha: String
 )
-
